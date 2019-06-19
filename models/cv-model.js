b2c6228ecf808c32ea9d1bf675729ebf7d7e3b2b
Albino29 <bruhalbino29@gmail.com>
2019-06-19 17:00:06 -0300

    Initial commit

---
+++ models/cv-model.js
@@ -0,0 +1,32 @@
+const data = {
+  title: 'Meu currículo',
+  name: 'Bruna Albino',
+  profession: 'Digital Developer',
+  description: '-',
+  experience: [
+    {
+      company: 'Renault - ADH',
+      office: 'Digital Developer',
+      description:
+        'Lab de inovação da Renault para projetos regionais e globais'
+    },
+    {
+      company: 'Brasil CT',
+      office: 'Desenvolvedora',
+      description: 'Desenvolvimento de integrações Rest / SOAP / em Java'
+    }
+  ],
+  education: [
+    {
+      institution: 'FIAP',
+      description: 'MBA em Arquitetura de Soluções'
+    },
+    {
+      institution: 'FIAP',
+      description: 'Análise e Desenvolvimento de Sistemas'
+    }
+  ],
+  skills: ['backend', 'frontend (ás vezes)', 'mobile (no futuro)']
+};
+
+module.exports = data;
